208bef0667a3560d95d6a363b384b4a9459581f1
Torauth <65465273+Torauth@users.noreply.github.com>
2020-11-27 17:00:44 +0000

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -4,12 +4,6 @@ import sqlite3
 PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
-def open_connection():
-  connection = getattr(g, '_connection', none)
-  if connection = None:
-    connection = g._connection = sqlite3.connect(PATH)  
-  connection.row_factory = sqlite3.Row
-  return connection
 
 @app.route('/')
 @app.route('/jobs')
